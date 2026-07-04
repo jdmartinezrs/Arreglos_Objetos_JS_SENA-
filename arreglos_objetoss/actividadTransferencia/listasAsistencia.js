3294ae066bd65526e33c7bea16a6502ed80a1669
jdmartinezrs <azrojas123@gmail.com>
2026-07-04 16:57:24 -0500

    Lista de asistencia: Cree un arreglo con los nombres de cinco aprendices. Agregue un nuevo nombre al final y elimine el primero. Finalmente, muestre la cantidad total de nombres registrados

--- arreglos_objetoss/actividadTransferencia/listasAsistencia.js
+++ arreglos_objetoss/actividadTransferencia/listasAsistencia.js
@@ -1,6 +1,6 @@
-/**1. Lista de asistencia: Cree un arreglo con los nombres de cinco aprendices. Agregue
+/**1. git commit -m "Lista de asistencia: Cree un arreglo con los nombres de cinco aprendices. Agregue
 un nuevo nombre al final y elimine el primero. Finalmente, muestre la cantidad total
-de nombres registrados. */
+de nombres registrados" */
 
 
 export const aniadirAlFinalListaAsistencia = (alumnos, alumno) => {
