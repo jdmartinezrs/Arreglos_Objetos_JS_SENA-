81adfbb17141273b1621fc27512a4ff7dc929856
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2026-07-07 19:59:57 +0000

    Control de edades: Cree un arreglo con edades de cinco personas. Recorra el arreglo usando for...of y muestre solo las edades mayores o iguales a 18

--- arreglos_objetoss/actividadTransferencia/controlEdad.js
+++ arreglos_objetoss/actividadTransferencia/controlEdad.js
@@ -1,5 +1,5 @@
-/**7. Control de edades: Cree un arreglo con edades de cinco personas. Recorra el
-arreglo usando for...of y muestre solo las edades mayores o iguales a 18.*/
+/**7. git commit -m "Control de edades: Cree un arreglo con edades de cinco personas. Recorra el
+arreglo usando for...of y muestre solo las edades mayores o iguales a 18".*/
 
 export const mostrarEdadesMayoresOIgualesDiezOcho = (edadesPersonas,edad) => {
 for (let edad of edadesPersonas){
