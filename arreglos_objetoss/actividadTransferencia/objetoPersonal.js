b3d9c29d3b6920f70943e61365b81b65586e058c
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2026-07-07 16:08:49 +0000

    6. Describiendo un objeto personal: Cree un objeto que represente algo que lleve consigo todos los días (ej. audífonos, libreta, termo). Debe tener al menos cuatro propiedades. Recorra sus claves y valores usando un ciclo for...of sobre Object.entries().

--- arreglos_objetoss/actividadTransferencia/objetoPersonal.js
+++ arreglos_objetoss/actividadTransferencia/objetoPersonal.js
@@ -1,8 +1,8 @@
 /**
- * 6. Describiendo un objeto personal: Cree un objeto que represente algo que lleve
+ * git commit -m "6. Describiendo un objeto personal: Cree un objeto que represente algo que lleve
 consigo todos los días (ej. audífonos, libreta, termo). Debe tener al menos cuatro
 propiedades. Recorra sus claves y valores usando un ciclo for...of sobre
-Object.entries().
+Object.entries()."
 
  */
 
