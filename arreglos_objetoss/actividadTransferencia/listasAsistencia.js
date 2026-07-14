5e7c09a4adff5ab4d5e07e8addd73cff4bd6a1a0
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2026-07-14 15:53:49 +0000

    feat(structure) cambio de nombre archivos y creación de objeto productos

--- arreglos_objetoss/actividadTransferencia/listasAsistencia.js
+++ arreglos_objetoss/actividadTransferencia/listasAsistencia.js
@@ -1,10 +1,11 @@
-/**1. git commit -m "Lista de asistencia: Cree un arreglo con los nombres de cinco aprendices. Agregue
-un nuevo nombre al final y elimine el primero. Finalmente, muestre la cantidad total
-de nombres registrados" */
+/**9. Arreglo de objetos (integración): Cree un arreglo llamado productos que contenga
+tres objetos. Cada objeto debe representar un producto con propiedades como
+nombre, precio y disponibilidad. Recorra el arreglo y muestre solo el nombre de
+cada producto. */
 
 
-export const aniadirAlFinalListaAsistencia = (alumnos, alumno) => {
-  return alumnos.push(alumno);
+export const recorreoArregloostrandoNombreProducto = (procutos) => {
+  
 }
 
 export const eliminarPrimerNombreListaAsistencia = (alumnos) => {
--- arreglos_objetoss/index.js
+++ arreglos_objetoss/index.js
@@ -1,6 +1,8 @@
 import { aniadirAlFinalListaAsistencia, eliminarPrimerNombreListaAsistencia, mostrarTotalNombresRegistrados  } from './actividadTransferencia/index.js'
 
-let listaEstudiantes = ["Jose", "Victor", "Marlon", "Juan", "Luisa"]
+let productos = [{nombre: "Gafas" , precio: 500000, disponibilidad: true },
+    {nombre: "Camiseta selección Colombia " , precio: 600000, disponibilidad: true},
+    {nombre: "Álbum Panini" , precio: 70000, disponibilidad: false }]
 
 let aniadirAlFinal = aniadirAlFinalListaAsistencia(listaEstudiantes, "Karol")
 
