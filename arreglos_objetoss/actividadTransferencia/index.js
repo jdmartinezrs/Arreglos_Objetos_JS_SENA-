1cf91f098bdb4922711bd875e6d814bb46a0c40b
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2026-07-14 16:15:37 +0000

    Arreglo de objetos (integración): Cree un arreglo llamado productos que contenga tres objetos. Cada objeto debe representar un producto con propiedades como nombre, precio y disponibilidad. Recorra el arreglo y muestre solo el nombre de cada producto.

--- arreglos_objetoss/actividadTransferencia/index.js
+++ arreglos_objetoss/actividadTransferencia/index.js
@@ -1,1 +1,1 @@
-export * from './listasAsistencia.js';
+export * from './objetoPersonal.js';
--- arreglos_objetoss/index.js
+++ arreglos_objetoss/index.js
@@ -1,16 +1,17 @@
-import { aniadirAlFinalListaAsistencia, eliminarPrimerNombreListaAsistencia, mostrarTotalNombresRegistrados  } from './actividadTransferencia/index.js'
+import { recorrerClavesValoresObjetoPersonal  } from './actividadTransferencia/index.js'
 
 let productos = [{nombre: "Gafas" , precio: 500000, disponibilidad: true },
     {nombre: "Camiseta selección Colombia " , precio: 600000, disponibilidad: true},
     {nombre: "Álbum Panini" , precio: 70000, disponibilidad: false }]
 
-let aniadirAlFinal = aniadirAlFinalListaAsistencia(listaEstudiantes, "Karol")
 
-let eliminarPrimero = eliminarPrimerNombreListaAsistencia(listaEstudiantes)
+let mapNombres = recorrerClavesValoresObjetoPersonal(productos)
 
-let mostrarCantidadTotal = mostrarTotalNombresRegistrados(listaEstudiantes)
-
+console.log( mapNombres);
 
 
+/**let registros = [{idUser:1, userName:'user1'},{idUser:2, userName:'user2'}, {idUser:3, userName:'user3'}]
 
+let mapIdUsers = registros.map(({idUser}) => idUser);
 
+console.log(mapIdUsers); */
